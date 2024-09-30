fd7efa25a6cfc22ca373711ce5aa568d086bf268
OfficialARM17 <127400865+OfficialARM17@users.noreply.github.com>
2024-09-30 21:46:39 +0100

    Update challenge_1_task_2.py

--- challenge_1_task_2.py
+++ challenge_1_task_2.py
@@ -1,14 +1,17 @@
 # Challenge 1 #
 # Task 2a #
+# Asking a user's name #
 name = input("What is your name? ")
+#  Outputting the inputted name #
 print(f"Hello {name}")
 # Task 2b #
 name = input("What is your name? ")
+# Use of If/Else Statement
 if(name == "Alice" or name == "Bob"):
   print(f"Hello {name}")
 else:
   print("Sorry... You're not authorised to be greeted")
-
+# All of my Answers for the question of Task 3 #
 # Task 3 #
 # 1. A #
 # 2. A #
